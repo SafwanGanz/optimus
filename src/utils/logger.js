43e1d6d7d03e1e12755db3a0e31ac6536b6589ce
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2025-09-01 21:01:42 +0530

    Enhance logger configuration with custom options

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,4 +2,31 @@ const pino = require('pino');
 
 module.exports = pino({
   level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'hostname,pid'
+    }
+  },
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    },
+  },
+  customLevels: {
+    message: 30,
+    error: 50
+  },
+  base: null,
+  timestamp: () => `,"time":"${new Date().toISOString()}"`,
+  messageKey: 'msg',
+  mixin: () => {
+    return { module: 'optimus' };
+  },
+  redact: {
+    paths: ['*.buffer', '*.ephemeralKeyPair', '*.signedKeyPair', '*.signedPreKey', '*.keyPair', '*._events', '*._eventsCount', '*.attrs'],
+    remove: true
+  }
 });
