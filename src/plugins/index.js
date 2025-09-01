ae8713d72680468a54e73cbe4653b0bc4ca4d598
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2025-09-01 17:03:54 +0530

    Update index.js

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,5 +2,6 @@ module.exports = [
   ...require('./admin/index'),
   ...require('./group/index'),
   ...require('./moderation/index'),
+  ...require('./media/index'),
   ...require('./utilities/index')
 ];
